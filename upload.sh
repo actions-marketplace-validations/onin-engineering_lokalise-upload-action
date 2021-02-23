d8a9334db64471740158d772faf11250d2b7eefd
Alex <alex@onin.co>
2021-02-23 11:46:56 +0000

    added action.yml

--- upload.sh
+++ upload.sh
@@ -1,1 +1,15 @@
-echo "Goodbye"
+echo ${file_path}
+set -ex
+
+echo ${lokalise_token}
+echo ${lokalise_project_id}
+
+brew tap lokalise/cli-2
+brew install lokalise2
+
+lokalise2 \
+    file upload \
+    --file ${file_path} \
+    --lang-iso ${lang_iso} \
+    --token ${lokalise_token} \
+    --project-id ${lokalise_project_id}
