3124f2a6c88088897e60114c4fe6c431ffa032f0
Alex <alex@onin.co>
2021-02-23 13:07:43 +0000

    fix upload arguments

--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
+set -ex
+
 while getopts t:p:f:l: flag
 do
     case "${flag}" in
@@ -8,18 +10,16 @@ do
     esac
 done
 
-echo ${file_path}
-set -ex
-
-echo ${lokalise_token}
-echo ${lokalise_project_id}
+echo $file_path
+echo $lokalise_token
+echo $lokalise_project_id
 
 brew tap lokalise/cli-2
 brew install lokalise2
 
 lokalise2 \
     file upload \
-    --file ${file_path} \
-    --lang-iso ${lang_iso} \
-    --token ${lokalise_token} \
-    --project-id ${lokalise_project_id}
+    --file $file_path \
+    --lang-iso $lang_iso \
+    --token $lokalise_token \
+    --project-id $lokalise_project_id
