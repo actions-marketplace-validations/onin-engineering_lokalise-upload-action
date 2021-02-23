c959b239c6460595db9fe2828787ddbd60ceadf8
Alex <alex@onin.co>
2021-02-23 13:17:29 +0000

    attempt using raw arguments

--- upload.sh
+++ upload.sh
@@ -1,25 +1,25 @@
 set -ex
 
-while getopts t:p:f:l: flag
-do
-    case "${flag}" in
-        t) lokalise_token=${OPTARG};;
-        p) lokalise_project_id=${OPTARG};;
-        f) file_path=${OPTARG};;
-        l) lang_iso=${OPTARG};;
-    esac
-done
+# while getopts t:p:f:l: flag
+# do
+#     case "${flag}" in
+#         t) lokalise_token=${OPTARG};;
+#         p) lokalise_project_id=${OPTARG};;
+#         f) file_path=${OPTARG};;
+#         l) lang_iso=${OPTARG};;
+#     esac
+# done
 
-echo $file_path
-echo $lokalise_token
-echo $lokalise_project_id
+# echo $file_path
+# echo $lokalise_token
+# echo $lokalise_project_id
 
 brew tap lokalise/cli-2
 brew install lokalise2
 
 lokalise2 \
     file upload \
-    --file $file_path \
-    --lang-iso $lang_iso \
-    --token $lokalise_token \
-    --project-id $lokalise_project_id
+    --file $3 \
+    --lang-iso $4 \
+    --token $1 \
+    --project-id $2
